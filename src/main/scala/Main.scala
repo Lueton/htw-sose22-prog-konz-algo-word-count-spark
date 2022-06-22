c1d70b1a6940ab141bfaaf0abef0d54eb328f995
Leon Lüttger <leon.luettger@web.de>
2022-06-22 18:01:11 +0200

    added scala and native counter

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,55 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import java.util.concurrent.TimeUnit
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+
+    val t0 = System.nanoTime
+    println("Result (native-all): " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime - t0) + "s")
+    val resultAllNative = NativeCounter.countWords(10)
+    resultAllNative.zipWithIndex.foreach { case (item, index) =>
+      println("" + (index + 1) + ". " + item._1 + " (" + item._2 + "x)")
+    }
+
+    val t1 = System.nanoTime
+    val resultsByLanguageNative = NativeCounter.countWordsByLanguage()
+    println("Result (native-by-language): " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime - t1) + "s")
+    resultsByLanguageNative.foreach(x => {
+      println(x._1)
+      x._2.zipWithIndex.foreach { case (item, index) =>
+        println("" + (index + 1) + ". " + item._1 + " (" + item._2 + "x)")
+      }
+      println("")
+    })
+
+    val t2 = System.nanoTime
+    val resultsAll = ScalaCounter.countWords();
+    println("Result (scala-all): " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime - t2) + "s")
+    resultsAll.zipWithIndex.foreach { case (item, index) =>
+      println("" + (index + 1) + ". " + item._1 + " (" + item._2 + "x)")
+    }
+
+    val t3 = System.nanoTime
+    val resultsByLanguage = ScalaCounter.countWordsByLanguage()
+    println("Result (scala-by-language): " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime - t3) + "s")
+    resultsByLanguage.foreach(x => {
+      println(x._1)
+      x._2.zipWithIndex.foreach { case (item, index) =>
+        println("" + (index + 1) + ". " + item._1 + " (" + item._2 + "x)")
+      }
+      println("")
+    })
   }
-  val conf = new SparkConf()
-    .setAppName("WordCounter")
-    .setMaster("local[*]")
-  val sc = new SparkContext(conf)
-  val file = sc.textFile("texts")
-  val map = CounterUtils.countWords(file)
-  map.saveAsTextFile("output/spark_output")
-  sc.stop()
 }
+
+
+
+
+/*
+var linesRdd = context.textFile("/Users/lueton/IdeaProjects/htw-sose22-prog-konz-algo-word-count-spark/texts/German/Casanovas Heimfahrt - Arthur Schnitzler.txt")
+var words1 = linesRdd.filter(x => x.nonEmpty).flatMap(x => x.toLowerCase().split("\\P{L}+"))
+words1.take(100).foreach(f => println(f))
+var wordsKv  = words1.filter(!broadcastStopWords.value.contains(_)).map(x => (x, 1)).reduceByKey(_ + _)
+var output1 = wordsKv
+println(output1.sortBy(_._2, ascending = false).take(10).mkString("Array(", ", ", ")"))
+context.stop()
+*/
---
+++ src/main/scala/ScalaCounter.scala
@@ -0,0 +1,33 @@
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object ScalaCounter extends Counter {
+  val sc: SparkContext = new SparkContext(new SparkConf().setAppName("ScalaCounter").setMaster("local[*]"))
+
+  override def countWords(limit: Int = 10): List[(String, Int)] = {
+    count(readFiles(), limit)
+  }
+
+  override def countWordsByLanguage(limit: Int = 10): Map[String, List[(String, Int)]] = {
+    val textFiles = readFiles().map(x => (x._1.split("/").dropRight(1).last, x._2)).cache()
+    val languages = textFiles.map(x => x._1).distinct.collect
+    languages.map(l => (l, count(textFiles.filter(_._1.contains(l)), limit))).toMap
+  }
+
+  def count(files: RDD[(String, String)], limit: Int): List[(String, Int)] = {
+    val stopWords: Broadcast[Set[String]] = sc.broadcast(sc.textFile(getClass.getResource("/stopwords/stopwords.txt").getPath).flatMap(x => x.split("\\P{L}+")).collect.toSet)
+    val words = files.flatMap(x => x._2.toLowerCase().split("\\P{L}+"))
+    val filteredWords = words.filter(!stopWords.value.contains(_))
+    val wordsWithCount = filteredWords.map(x => (x, 1)).reduceByKey(_ + _)
+    val sortedWordsWithCount = wordsWithCount.sortBy(_._2, ascending = false)
+    sortedWordsWithCount.take(limit).toList
+  }
+
+  def readFiles () : RDD[(String, String)] = {
+    val files = sc.wholeTextFiles(getClass.getResource("/texts").getPath + "/*")
+     val e = files.filter((file) => file._1.contains(".txt")).cache()
+    println(e.count)
+    e
+  }
+}
