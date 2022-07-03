db1cebd12a54bbbfcf331d9ddbbc53cae6da3b2f
Malte Gronwald <gronwaldmalte@gmail.com>
2022-07-03 15:38:41 +0200

    add deleteQuietly

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,24 @@
+import org.apache.hadoop.shaded.org.apache.commons.io.FileUtils
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+
+import java.io.File
+
+
+
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
   }
-  val conf = new SparkConf()
+  val outputPath = "output/spark_output"
+  FileUtils.deleteQuietly(new File(outputPath))
+  val conf: SparkConf = new SparkConf()
     .setAppName("WordCounter")
     .setMaster("local[*]")
   val sc = new SparkContext(conf)
-  val file = sc.textFile("texts")
-  val map = CounterUtils.countWords(file)
-  map.saveAsTextFile("output/spark_output")
+  val file: RDD[String] = sc.textFile("texts/Dutch")
+  val map: RDD[(String, Int)] = CounterUtils.countWords(file)
+  map.saveAsTextFile(outputPath)
   sc.stop()
 }
