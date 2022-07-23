f9f093796f8fec99debb1dfead09a0f1d10b91b3
Leon Lüttger <leon.luettger@web.de>
2022-07-23 16:29:45 +0200

    added tests

--- src/test/scala/ScalaCounterTest.scala
+++ src/test/scala/ScalaCounterTest.scala
@@ -1,15 +1,36 @@
 import org.scalatest.flatspec.AnyFlatSpec
 
-import scala.collection.mutable
-
 class ScalaCounterTest extends AnyFlatSpec{
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new mutable.Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    assert(stack.pop() === 2)
-    assert(stack.pop() === 1)
+  "The result" should "not include any stop or empty word for all words" in {
+    val result = ScalaCounter.countWords(10, "src/test/resources/texts", "src/test/resources/stopwords.txt")
+    val words = result.flatMap(_._1)
+    assert(words.contains("und") === false)
+    assert(words.contains("and") === false)
+    assert(words.contains("") === false)
   }
 
+  "The result" should "include correct word count for all words" in {
+    val result = ScalaCounter.countWords(10, "src/test/resources/texts", "src/test/resources/stopwords.txt")
+    assert(result.exists((entry) => entry._1 === "ich" && entry._2 === 3))
+    assert(result.exists((entry) => entry._1 === "me" && entry._2 === 3))
+    assert(result.exists((entry) => entry._1 === "are" && entry._2 === 1))
+  }
+
+  "The result" should "not include any stop or empty word for words by language" in {
+    val result = ScalaCounter.countWordsByLanguage(10, "src/test/resources/texts", "src/test/resources/stopwords.txt")
+    val wordsGerman = result("German").flatMap(_._1)
+    val wordsEnglish = result("English").flatMap(_._1)
+    assert(wordsGerman.contains("und") === false)
+    assert(wordsEnglish.contains("and") === false)
+    assert(wordsGerman.contains("") === false)
+    assert(wordsEnglish.contains("") === false)
+  }
+
+  "The result" should "include correct word count for words by language" in {
+    val result = ScalaCounter.countWordsByLanguage(10, "src/test/resources/texts", "src/test/resources/stopwords.txt")
+    assert(result("German").exists((entry) => entry._1 === "ich" && entry._2 === 3))
+    assert(result("English").exists((entry) => entry._1 === "me" && entry._2 === 3))
+    assert(result("English").exists((entry) => entry._1 === "are" && entry._2 === 1))
+  }
 }
